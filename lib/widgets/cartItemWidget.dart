00bd8332628e9e00ca01a5a65e8203b85177efae
Vitor Lucas Crispim <vitorcrispim@yahoo.com.br>
2021-04-16 15:32:43 -0300

    Adicionado a estilização ao card para remover o item,e chamando a função que remove o item

--- lib/widgets/cartItemWidget.dart
+++ lib/widgets/cartItemWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -6,14 +7,41 @@ class CartItemWidget extends StatelessWidget {
   CartItemWidget(this.cartItem);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: Text(cartItem.title),
-          subtitle: Text('Total : R\$${cartItem.price * cartItem.quantity}'),
-          trailing: Text('${cartItem.quantity}x'),
+    //Dissmissible adiciona uma rolagem a lista
+    return Dismissible(
+      //remove o item "arrastado" pelo user
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId);
+      },
+      key: ValueKey(cartItem.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: Text(cartItem.title),
+            subtitle: Text('Total : R\$${cartItem.price * cartItem.quantity}'),
+            trailing: Text('${cartItem.quantity}x'),
+          ),
         ),
       ),
     );
